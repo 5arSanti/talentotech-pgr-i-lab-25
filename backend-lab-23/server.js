e9e5dab090662847b9198ae5c660802f1b9c4717
5arSanti <santiari05@hotmail.com>
2024-11-30 19:58:30 -0500

    New noticias endpoint

--- backend-lab-23/server.js
+++ backend-lab-23/server.js
@@ -45,4 +45,35 @@ app.get("/users", (requeset, response) => {
     }
 })
 
+app.get("noticias", (request, response) => {
+    try {
+        const noticias = [{
+            titulo: "Descubren nueva especie de dinosaurio",
+            descripcion: "Paleontólogos han descubierto una nueva especie de dinosaurio en Argentina.",
+            fecha: "2023-10-01"
+        }, {
+            titulo: "Lanzamiento del nuevo iPhone",
+            descripcion: "Apple ha lanzado su nuevo iPhone con características innovadoras.",
+            fecha: "2023-10-02"
+        }, {
+            titulo: "Avances en la cura del cáncer",
+            descripcion: "Científicos han hecho avances significativos en la búsqueda de una cura para el cáncer.",
+            fecha: "2023-10-03"
+        }, {
+            titulo: "Cambio climático y sus efectos",
+            descripcion: "Expertos advierten sobre los efectos del cambio climático en el planeta.",
+            fecha: "2023-10-04"
+        }, {
+            titulo: "Nuevas medidas contra el COVID-19",
+            descripcion: "Gobiernos alrededor del mundo implementan nuevas medidas para combatir el COVID-19.",
+            fecha: "2023-10-05"
+        }];
+
+        return response.status(200).json(noticias);
+    } 
+    catch (err) {
+        return response.status(500).json({ Error: "Ocurrio un error en el servidor"})
+    }
+})
+
 module.exports = app;
