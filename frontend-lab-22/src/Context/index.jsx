f67967735c2c08c58a48d6ead6ce30a3256489ca
5arSanti <santiari05@hotmail.com>
2024-11-30 19:45:54 -0500

    Get info from server usign effect

--- frontend-lab-22/src/Context/index.jsx
+++ frontend-lab-22/src/Context/index.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { handleFetch } from '../Utils/handleFetch';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
     const [responseData, setResponseData] = React.useState(null);
 
-    
+    React.useEffect(() => {
+        handleFetch("users", setResponseData)
+    }, [])
 
     return(
         <AppContext.Provider value={{
